6fe52c9736f88e23acb6ce003b27502232d73929
EdwardBonner96 <edwardo.bonner@gmail.com>
2022-12-16 23:55:44 +0000

    Absorb unneeded functions into game function

--- run.py
+++ run.py
@@ -61,31 +61,9 @@ selected_word = words_list[word_index].upper()
 print(selected_word) #test, delete later
 
 for i in range(len(selected_word)):
-    guesses = guesses + "_ "
+    guesses = guesses + "_"
 
 print(guesses) #test, delete later
-
-
-def user_select_letter():
-    """
-    Get letter input from the user.
-    """
-    print("===============")
-    print("Please Select a letter")
-    print(guesses)
-    user_input = input("Enter letter here: ")
-
-    return user_input
-
-
-def validate_user_input(user_input):
-    """
-    Validates user input, only one letter accepted.
-    """
-    if user_input.isalpha() == False or len(user_input) != 1:
-        print("===============")
-        print("Invalid Input")
-        print(f"{lives} lives remaining")
 
 
 def print_hangman_state():
@@ -98,17 +76,24 @@ def print_hangman_state():
 
 while lives > 0:
     print_hangman_state()
-    letter_selected = user_select_letter()
-    if letter_selected in selected_word:
+    print("===============")
+    print("Please Select a letter")
+    print(guesses)
+    user_input = input("Enter letter here: ").upper()
+    if user_input.isalpha() == False or len(user_input) != 1:
+        print("===============")
+        print("Invalid Input")
+        print(f"{lives} lives remaining")
+        continue
+    if user_input in selected_word:
         for i in range(len(selected_word)):
-            if letter_selected == selected_word[i]:
-                guesses[i] = letter_selected
+            if user_input == selected_word[i]:
+                guesses[i] = user_input
         if "_" not in guesses:
             break
     else:
         lives -= 1
-        print_hangman_state()
-        print(f"{letter_selected} is not in the word!")
+        print(f"\nThe letter {user_input} is not in the word!")
                 
 
 #  Shows win and loss messages on game over
