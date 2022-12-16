4c48dab22d50485c388349cdcefc9e949acddee2
EdwardBonner96 <edwardo.bonner@gmail.com>
2022-12-16 18:55:58 +0000

    Add function to receive user input

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-#Allows random number generation
+# Allows random number generation
 import random
 
-#ASCII art for lives
+# ASCII art for lives, from https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
 HANGMANPICS = ['''
   +---+
   |   |
@@ -55,6 +55,29 @@ HANGMANPICS = ['''
 
 words_list = ['testone', 'Testtwo', 'testthree']
 
-print(random.randint(0, len(words_list)-1))
 
-print(HANGMANPICS[1])
+def user_select_letter():
+    """
+    Get letter input from the user.
+    """
+    while True:
+        print("Please Select a letter\n")
+
+        user_input = input("Enter letter here: ")
+
+        if validate_user_input(user_input):
+            print("Test print valid data")
+            break
+
+    return user_input
+
+
+def validate_user_input(user_input):
+    """
+    Validates user input, one letter accepted.
+    """
+    if user_input.isalpha() == False or len(user_input) != 1:
+        print("Invalid Input\n")
+
+
+letter_selected = user_select_letter()
