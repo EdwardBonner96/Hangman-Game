037df9855022d6a613b7cc04374d83b57c3d7a27
EdwardBonner96 <edwardo.bonner@gmail.com>
2022-12-17 01:48:22 +0000

    Add guessed letter tracker

--- run.py
+++ run.py
@@ -59,6 +59,7 @@ lives = 6
 word_index = random.randint(0, len(words_list)-1)
 selected_word = words_list[word_index].upper()
 game_over_word = selected_word
+input_tracker = ""
 print(selected_word) #test, delete later
 
 for i in range(len(selected_word)):
@@ -79,14 +80,15 @@ while lives > 0:
     print_hangman_state()
     print("===============")
     print("Please Select a letter")
+    print(f"Guessed letters: {input_tracker}")
     print(guesses)
     user_input = input("Enter letter here: ").upper()
     if user_input.isalpha() == False or len(user_input) != 1:
         print("===============")
         print("Invalid Input")
         continue
-    if user_input in selected_word:
-        for i in range(1, len(selected_word)):
+    if user_input in game_over_word:
+        for i in range(0, len(selected_word)):
             if user_input == selected_word[i]:
                 x = selected_word.find(user_input) 
                 guesses = guesses[:x] + user_input + guesses[x+1:]
@@ -96,6 +98,11 @@ while lives > 0:
     else:
         lives -= 1
         print(f"\nThe letter {user_input} is not in the word!")
+    if user_input in input_tracker:
+        print(f"You have already guessed {user_input}!")
+        continue
+    input_tracker = input_tracker + user_input
+    print(input_tracker)
                 
 
 #  Shows win and loss messages on game over
